6a2723729d144f6ec88a19366bb9797310b93b1d
stepang <g.stepanek.nz@gmail.com>
2015-12-11 22:20:33 +0000

    Added offset url query param for paging

--- app/controllers/handler.server.js
+++ app/controllers/handler.server.js
@@ -4,11 +4,18 @@ var request = require('request');
 
 function Handler () {
 
-	// e.g. https://fcc-api-basejumps-stepang.c9users.io/api/imagesearch/lolcats%20funny
+	// e.g. https://fcc-api-basejumps-stepang.c9users.io/api/imagesearch/lolcats%20funny?offset=10
 	this.getImageSearch = function (req, res) {
 		
 		var url = "https://www.googleapis.com/customsearch/v1?searchType=image&key=" + process.env.GOOGLE_API_KEY +
 			"&cx=" + process.env.GOOGLE_SEARCH_ENGINE + "&q=" + req.params.searchterm;
+			
+		if(req.query.offset) {
+			var offset = parseInt(req.query.offset, 10);
+			if(!isNaN(offset) && offset >= 0) {
+				url += "&start=" + (offset + 1);
+			}
+		}
 			
 	  	request.get(url, {json: true}, function(err, result, body) {
 	      	if (err || result.statusCode != 200) {
